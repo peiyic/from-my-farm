3004e1fe91a3748f1d12a6abd7454f310b3d4333
8balance <132126388+8balance@users.noreply.github.com>
2023-07-25 22:39:54 +1200

    add navigation bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Map from '../components/map';
+import Navigation from '../components/navigation';
 
 export default function Home() {
   return (
     <div>
+      <Navigation />
       <Map />
     </div>
   )
---
+++ src/components/navigation.tsx
@@ -0,0 +1,10 @@
+"use client";
+import styles from './navigation.module.css'
+import {useState, useMemo} from 'react';
+
+const Navigation = () => {
+    return <div className={styles.navigation}>hello world</div>
+}
+
+
+export default Navigation;
