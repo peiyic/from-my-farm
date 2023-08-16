789b36ec76541dfb632e12d21de0a943b98b3758
8balance <132126388+8balance@users.noreply.github.com>
2023-08-17 00:47:04 +1200

    position title

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import styles from './logo.module.css'
 import {useState, useMemo} from 'react';
 
 const Logo = () => {
-    return <div className={styles.grid}>logo</div>
+    return <div className={styles.grid}><a className={styles.title} href='https://www.google.com'>From my Farm</a></div>
 }
 
 
