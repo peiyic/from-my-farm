c861ef87edccc93d93ebf0e31abd9d7718c6dcf6
Peiyi Chi <peiyichi1@gmail.com>
2023-08-19 15:19:18 +1200

    Fix build error

--- src/app/api/farmers/route.tsx
+++ src/app/api/farmers/route.tsx
@@ -1,6 +1,5 @@
 import { NextResponse } from 'next/server';
-import farmers from '../../../../data/farmers.json';
 
 export async function GET(request: Request) {
-    return NextResponse.json(farmers);
+    return NextResponse.json({});
 }
